56f6d95d3a3020fda5f170c399b24a5dda6c671d
Siddharth <siddharthkadu2001@gmail.com>
2024-08-23 16:49:20 +0530

    create a function for checking strength of password

---
+++ Password_Generator_Strength_Validator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <algorithm> // For count_if
+
+using namespace std;
+string generatePassword(int length) {
+    string password = ""; // To store the generated password
+    string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`! @#$%^&*"; // Characters set
+    int charSize = characters.size();
+    srand(static_cast<unsigned int>(time(0))); 
+
+    for (int i = 0; i < length; ++i) {
+        int randomIndex = rand() % charSize; // Get a random index
+        password += characters[randomIndex]; // Append the corresponding character
+    }
+
+    return password;
+}
+
+string evaluatePasswordStrength(const string& password) {
+    bool hasLower = any_of(password.begin(), password.end(), ::islower);
+    bool hasUpper = any_of(password.begin(), password.end(), ::isupper);
+    bool hasDigit = any_of(password.begin(), password.end(), ::isdigit);
+    bool hasSpecial = any_of(password.begin(), password.end(), [](char c){ return ispunct(c); });
+
+    int strengthScore = hasLower + hasUpper + hasDigit + hasSpecial;
+
+    if (strengthScore == 4 && password.length() >= 12) {
+        return "Strong";
+    } else if (strengthScore >= 3 && password.length() >= 8) {
+        return "Moderate";
+    } else {
+        return "Weak";
+    }
+}
